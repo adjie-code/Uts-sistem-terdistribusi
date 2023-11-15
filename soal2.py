314983aa6627b32ace2241c0750336d419813a4c
adjie-code <59765402+adjie-code@users.noreply.github.com>
2023-11-15 22:15:07 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+    if 1 <= n <= 150:
+        for i in range(1, n + 1):
+            print(i,end='')
+    else:
+        print("Angka lebih dari 150")
+
